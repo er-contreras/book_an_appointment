2d14239516f419d9aedb5c7e66add80ff65d202a
Erick Contreras <eri89--@hotmail.com>
2022-01-10 15:13:08 -0600

    Fix linters

--- app/controllers/api/v1/yachts_controller.rb
+++ app/controllers/api/v1/yachts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::YachtsController < ApplicationController
   def index
-    render json: Yacht.all, only: [:price_per_hour, :description, :max_time, :insurence, :extra_person_fee]
+    render json: Yacht.all, only: %i[price_per_hour description max_time insurence extra_person_fee]
   end
 end
