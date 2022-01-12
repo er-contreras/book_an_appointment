3ab94e8702fbcbb63adb410e759006946308391a
Meltrust <original.mtapia@outlook.com>
2022-01-12 08:24:31 -0600

    Update racks cors config

--- app/controllers/api/v1/yachts_controller.rb
+++ app/controllers/api/v1/yachts_controller.rb
@@ -3,3 +3,7 @@ class Api::V1::YachtsController < ApplicationController
     render json: Yacht.all, only: %i[price_per_hour description max_time insurence extra_person_fee]
   end
 end
+
+def create; end
+
+def show; end
