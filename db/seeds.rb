b8034da840df3a8534192d06916d957ee9362205
Meltrust <original.mtapia@outlook.com>
2022-01-19 13:44:57 -0600

    Add more default yachts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,34 @@ Yacht.create(
     { price_per_hour: 1,
       description: 'This yacht is amazing!',
       max_time: 3,
-      insurence: 400,
-      extra_person_fee: 500 }
+      insurance: 400,
+      extra_person_fee: 500,
+      name: 'Rondon',
+      reserved: false },
+
+    { price_per_hour: 4,
+      description: 'The king of the sea!',
+      max_time: 5,
+      insurance: 600,
+      extra_person_fee: 400,
+      name: 'King',
+      reserved: false },
+
+    { price_per_hour: 20,
+      description: 'The most luxurious yacht in the world',
+      max_time: 5,
+      insurance: 400,
+      extra_person_fee: 500,
+      name: 'Siren',
+      reserved: false },
+
+    { price_per_hour: 5,
+      description: 'Our fastest yacht',
+      max_time: 2,
+      insurance: 400,
+      extra_person_fee: 600,
+      name: 'Pearl',
+      reserved: false }
+
   ]
 )
