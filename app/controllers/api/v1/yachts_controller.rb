41df925c7a63f18b50ab17eaff2d1a53c78156fc
Meltrust <original.mtapia@outlook.com>
2022-01-19 16:36:44 -0600

    Refactor index and show controller

--- app/controllers/api/v1/yachts_controller.rb
+++ app/controllers/api/v1/yachts_controller.rb
@@ -1,13 +1,20 @@
 class Api::V1::YachtsController < ApplicationController
   def index
-    render json: Yacht.all, only: %i[price_per_hour name description max_time insurence extra_person_fee]
+    @yachts = Yacht.all.map { |e| e.attributes.merge(picture: rails_blob_path(e.picture)) }
+    render json: { data: @yachts }, status: 200
+
+    # data = { yachts: Yacht.all.as_json, pictures: rails_blob_path }
+
+    # render json: Yacht.all, only: %i[price_per_hour name description max_time insurence extra_person_fee]
   end
 
   def create; end
 
   def show
-    yacht = Yacht.find_by(name: params[:id])
-    render json: yacht
+    item = Yacht.find_by(name: params[:id])
+    yacht = item.attributes.merge(picture: rails_blob_path(item.picture))
+    # picture = rails_blob_path(yacht.picture)
+    render json: { yacht: yacht }, status: 200
   end
 
   private
