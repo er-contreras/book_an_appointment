54c0269a00c9fd1f91e1dbb6c23aaba7abc9ddec
Meltrust <original.mtapia@outlook.com>
2022-01-20 23:44:10 -0600

    Add Yacht validations

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,3 +1,9 @@
 class Yacht < ApplicationRecord
   has_one_attached :picture
+
+  validates :name, presence: true
+  validates :price_per_hour, presence: true
+  validates :description, presence: true
+  validates :insurance, presence: true
+  validates :extra_person_fee, presence: true
 end
