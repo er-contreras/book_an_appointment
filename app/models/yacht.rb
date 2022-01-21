77d0514be384ed6675923a943c57ad51d642c89a
Meltrust <original.mtapia@outlook.com>
2022-01-21 01:01:58 -0600

    Add max length for yacht descriptions

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -4,7 +4,7 @@ class Yacht < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   validates :price_per_hour, presence: true, numericality: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
   validates :insurance, presence: true, numericality: true
   validates :extra_person_fee, presence: true, numericality: true
 end
