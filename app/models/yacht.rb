83f9c3e45b30d6a8c8984c2d3b8a5b1b60edb6fe
Meltrust <original.mtapia@outlook.com>
2022-01-21 01:20:08 -0600

    Add validation for max time

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,10 +1,11 @@
 class Yacht < ApplicationRecord
   has_one_attached :picture
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
 
-  validates :price_per_hour, presence: true, numericality: true
+  validates :price_per_hour, presence: true, numericality: { in: 1_000_000 }
   validates :description, presence: true, length: { maximum: 500 }
-  validates :insurance, presence: true, numericality: true
-  validates :extra_person_fee, presence: true, numericality: true
+  validates :insurance, presence: true, numericality: { in: 0..1_000_000 }
+  validates :extra_person_fee, presence: true, numericality: { in: 0..1_000_000 }
+  validates :max_time, presence: true, numericality: { in: 1..100 }
 end
