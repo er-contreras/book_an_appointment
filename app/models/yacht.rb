b9e2c8ae2806341447246cc4adb2eb18c5f0a9d6
Meltrust <original.mtapia@outlook.com>
2022-01-21 03:46:55 -0600

    Fix linter

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -2,7 +2,6 @@ class Yacht < ApplicationRecord
   has_one_attached :picture
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-
   validates :price_per_hour, presence: true, numericality: { in: 1_000_000 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :insurance, presence: true, numericality: { in: 0..1_000_000 }
