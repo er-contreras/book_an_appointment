f8da98fafb8a48e60fc0d6133f7a037c09114960
Meltrust <original.mtapia@outlook.com>
2022-01-21 09:06:57 -0600

    Correct price validation

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -3,7 +3,7 @@ class Yacht < ApplicationRecord
   validate :picture_present?
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-  validates :price_per_hour, presence: true, numericality: { in: 1_000_000 }
+  validates :price_per_hour, presence: true, numericality: { in: 0..1_000_000 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :insurance, presence: true, numericality: { in: 0..1_000_000 }
   validates :extra_person_fee, presence: true, numericality: { in: 0..1_000_000 }
