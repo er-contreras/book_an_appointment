08e4e8a70cb132de2aa91aa213ee9b9e3a87beb0
Meltrust <original.mtapia@outlook.com>
2022-01-21 07:09:32 -0600

    Add picture validation

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,5 +1,6 @@
 class Yacht < ApplicationRecord
   has_one_attached :picture
+  validate :picture_present?
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :price_per_hour, presence: true, numericality: { in: 1_000_000 }
@@ -7,4 +8,10 @@ class Yacht < ApplicationRecord
   validates :insurance, presence: true, numericality: { in: 0..1_000_000 }
   validates :extra_person_fee, presence: true, numericality: { in: 0..1_000_000 }
   validates :max_time, presence: true, numericality: { in: 1..100 }
+
+  private
+
+  def picture_present?
+    errors.add(:picture, :blank) unless picture.attached?
+  end
 end
